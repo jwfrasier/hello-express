9410ad52b934414c6f67b60cfe68de03cd8d92a9
Joseph Frasier <frasier.joseph@gmail.com>
2017-08-16 14:56:00 -0500

    Sucky css but good directory

--- app.js
+++ app.js
@@ -7,40 +7,11 @@ app.set('view engine', 'mustache')
 app.use(express.static('public'))
 
 app.get("/", function(request, response) {
-  const title = "Express is super fun!"
   response.render('index', {
-    title: title,
-    people: data.users,
-    avatar: data.users.avatar
-  })
-})
-app.get("/ohhai", function(request, response) {
-  response.send("OH HAI THERE")
-})
-
-app.get('/people/:id', function(request, response) {
-
-  // console.log(request)
-  // response.send("WELCOME TO BOB MARLEY")
-  // response.send( request.params )
-
-  // Find the person from the people array
-  // where the id is the request.params.id
-  const idWeWant = parseInt(request.params.id)
-  let person = false;
-  for (var i = 0; i < data.people.length; i++) {
-    if (data.people[i].id === idWeWant) {
-      person = data.people[i]
-    }
-  }
-
-  response.render("person", {
-    person: person
+    robots: data.users
   })
 
 })
-
-
 app.listen(3000, function() {
   console.log("Express started on port 3000")
 })
